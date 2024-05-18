e76dd7ca2cdfb654d62493bb15fe6e4e3e08aa1c
Seol SO <hi@cattynip.dev>
2024-05-17 21:29:30 -0400

    feat: add styles on the index page

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -1,11 +1,55 @@
-export default function NavBar() {
-    return (
-      <div className="flex flex-row p-3 m:p-6 lg:p-9 justify-between align-center">
-        <div className="font-bold">LOGO</div>
-        <div className="flex flex-row gap-3 m:gap-4.5 lg:gap-6">
-            <div>Join</div>
-            <div>SignIn</div>
-        </div>
+import Link from "next/link";
+
+interface NavBarReceive {
+  loggedIn: boolean;
+}
+
+function NavBar({ loggedIn }: NavBarReceive) {
+  return (
+    <nav className="px-5 py-2 fixed top-0 left-0 w-full flex items-center justify-between bg-transparent backdrop-blur-xl *:flex *:space-x-2 border-b-2 border-b-gray-200">
+      <div>
+        <span>LOGO!</span>
       </div>
-    );
-  }
+      <div>
+        <NavBarLink
+          linkTo="https://github.com/BY-Chai/Data-Hackfest-2024-Submission"
+          innerText="GitHub"
+          isExternal
+        />
+
+        {loggedIn ? (
+          <>
+            <NavBarLink linkTo="/profile" innerText="Profile" />
+          </>
+        ) : (
+          <>
+            <NavBarLink linkTo="/signup" innerText="Join" />
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+interface NavBarLinkReceive {
+  linkTo: string;
+  innerText: string;
+  isExternal?: boolean;
+}
+
+export function NavBarLink({
+  linkTo,
+  innerText,
+  isExternal,
+}: NavBarLinkReceive) {
+  return (
+    <Link
+      className="transition-colors py-1 px-3 shadow-black border border-transparent rounded-md hover:bg-opacity-40 hover:bg-gray-300 hover:border-gray-300 hover:shadow-sm"
+      href={linkTo}
+      target={isExternal ? "_blank" : "_parent"}>
+      <span>{innerText}</span>
+    </Link>
+  );
+}
+
+export default NavBar;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,52 @@
-import NavBar from "./components/NavBar";
+import Link from "next/link";
 
-export default function Home() {
+function Home() {
   return (
-    <div className="flex flex-col h-full">
-      <NavBar />
-      <div className="flex justify-center items-center h-full flex-col">
-        <h1 className="text-6xl font-bold">PathFinder</h1>
-        <p className="font-semibold p-2 sm:p-4 lg:p-6">No need to fret about directions or planning. Pathfinder takes the lead, taking care of all your needs ;-)</p>
+    <>
+      <div className="fixed top-0 left-0 w-full h-screen bg-gradient-to-t from-blue-200 via-transparent to-transparent -z-10" />
+      <div className="flex flex-col items-center justify-center space-y-10 pt-56">
+        <div className="flex justify-center items-center flex-col">
+          <Link
+            href="https://data-hackfest-21458.devpost.com/"
+            className="bg-black text-white py-1.5 px-4 rounded-full text-[13px]">
+            <span>DataHackfest 2024</span>
+          </Link>
+
+          <div className="text-5xl md:text-6xl lg:text-[75px] font-semibold text-left sm:text-center pt-2 space-y-0 sm:-space-y-2.5">
+            <h1>
+              <p>
+                Walk{" "}
+                <span className="font-black bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-yellow-500 ">
+                  every day
+                </span>
+              </p>
+              <p>
+                with{" "}
+                <span className="font-black bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-sky-400">
+                  safety
+                </span>{" "}
+                and{" "}
+                <span className="font-black bg-clip-text text-transparent bg-gradient-to-r from-orange-700 to-purple-500">
+                  your mood
+                </span>
+              </p>
+            </h1>
+
+            <p className="font-normal text-sm text-gray-500 text-left md:text-lg sm:text-center pt-2 sm:pt-6 pb-10">
+              No need to fret about directions or planning. Pathfinder takes the
+              lead, taking care of all your needs ;-)
+            </p>
+          </div>
+
+          <Link
+            className="transition-transform bg-gradient-to-r from-blue-800 to-purple-600 text-white py-3 px-6 rounded-lg hover:scale-105"
+            href="/signup">
+            Let's Walk!
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+export default Home;
